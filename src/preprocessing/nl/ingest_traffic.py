d884dca0b8c0b132c667f8db224088cb375ddb65
Erwin Torreao Dassen <erwin.dassen@stcorp.nl>
2016-10-07 15:40:07 +0200

    Added the time range of the given HDF dataset; Categorized the node_id; Minor bugfixes;

--- src/preprocessing/nl/ingest_traffic.py
+++ src/preprocessing/nl/ingest_traffic.py
@@ -4,6 +4,7 @@ Copyright S[&]T
 """
 
 from deco import concurrent, synchronized
+from boltons.iterutils import chunked_iter
 
 import begin
 
@@ -18,6 +19,8 @@ from collections import namedtuple
 from multiprocessing import cpu_count
 import logging
 
+
+DFS_PER_FILE = 5000
 
 # Define record type
 columns = ['node_id', 'timestamp', 'measurement', 'measurement_type']
@@ -96,8 +99,18 @@ def process(fpath):
 
     if len(data) > 0:
         df = pd.DataFrame(data=data, columns=columns).dropna()  # We could decide to do something else
+
         df = df.groupby(by=['timestamp', 'node_id', 'measurement_type'], as_index=False).mean()
         df = df.pivot_table(index=['timestamp', 'node_id'], columns=['measurement_type'], values=['measurement'])
+
+        # Pandas cannot write multiindex columns to hdf yet. We have to drop it.
+        df.columns = ['averageVehicleSpeed', 'vehicleFlowRate']
+
+        # Recast indices to collumns (also needed for dask)
+        df.reset_index(inplace=True)
+
+    else:
+        df = pd.DataFrame()  # We need to return a DataFrame object even if empty
 
     return df
 
@@ -132,12 +145,13 @@ def run(input:  'Input dir to look recursively for NDW traffic xml.gz files' = '
     globs = list(input.rglob('*raffic*.gz'))  # This gets relevant files of both types
 
     @synchronized
-    def orchestrate(globs, progress=True):
+    def orchestrate(globs):
         dfs = dict()
 
         # Loop XML Files
         for fpath in globs:
             dfs[fpath.name] = process(fpath)
+            # assert isinstance(dfs[fpath.name], pd.DataFrame)
 
         return dfs
 
@@ -147,21 +161,24 @@ def run(input:  'Input dir to look recursively for NDW traffic xml.gz files' = '
     # Throw into Dataframe
     if len(dfs) > 0:
 
-        df = pd.concat(dfs.values(), axis=0)
+        for sdfs in chunked_iter(dfs.values(), DFS_PER_FILE):
+            df = pd.concat(sdfs, axis=0)
 
-        # Drop Duplicates
-        df.drop_duplicates(inplace=True)
+            # Drop Duplicates
+            df.drop_duplicates(inplace=True)
 
-        # Pandas cannot write multiindex columns to hdf yet. We have to drop it.
-        df.columns = ['averageVehicleSpeed', 'vehicleFlowRate']
+            # Categorize node_id
+            df.node_id = df.node_id.astype('category')
 
-        # Recast indices to collumns (also needed for dask)
-        df.reset_index(inplace=True)
+            # Store Dataframe
+            fname = output / pl.Path('Traffic-{}.hdf5'.format(datetime.now().strftime('%y-%m-%d-%H-%M-%S-%f')))
+            logging.info("// Saving %i Records to %s" % (len(df), fname))
 
-        # Store Dataframe
-        fname = str(output / pl.Path('Traffic-{}.hdf5'.format(datetime.now().strftime('%y-%m-%d-%H-%M-%s'))))
-        logging.info("// Saving %i Records to %s" % (len(df), fname))
-        df.to_hdf(fname, key='df', mode='w', format='table')
+            with pd.HDFStore(str(fname), mode='w', format='table') as store:
+                store.put('df', df, format='table')
+                store.put('range', pd.Series([df.timestamp.min(), df.timestamp.max()]), format='table')
+
+            logging.info("// Done")
 
     else:
         logging.info("!! No Records. Not Saving.")
